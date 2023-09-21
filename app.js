efd81d472fff47b63c089cdf72c747f87d704984
dwarlock <92117358+movereCrus@users.noreply.github.com>
2023-09-21 19:50:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ let tg = window.Telegram.WebApp
 
 let wrap = document.getElementById("test")
 let p = document.createElement("p")
-p.innerText = tg.initData.user
+p.innerText = `$(tg.initDataUnsafe.user.first_name)`
 wrap.appendChild(p)
