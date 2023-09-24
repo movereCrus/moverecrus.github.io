bd9e78f156ce4105baa03a80151df1b46a2487cf
dwarlock <92117358+movereCrus@users.noreply.github.com>
2023-09-24 18:39:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,26 @@ let tg = window.Telegram.WebApp
 
 let wrap = document.getElementById("test")
 let p = document.createElement("p")
-console.log(tg.initDataUnsafe)
-p.innerText = `${tg.initDataUnsafe.user}`
+let initData = tg.initDataUnsafe
+console.log(initData)
+p.innerText = `${initData.user.first_name}`
 wrap.appendChild(p)
+
+let initData = tg.initDataUnsafe
+
+axios.post('localhost:8000/api/auth', {
+    auth_date: initData.auth_date,
+    user_id: initData.user.id,
+    first_name: initData.user.first_name,
+    last_name: initData.user.last_name,
+    username: initData.user.username,
+    language_code: initData.user.language_code,
+    allow_write_to_pm: initData.user.allow_write_to_pm,
+    hash: initData.hash,
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
